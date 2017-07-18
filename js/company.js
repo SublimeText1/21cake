3e17e89bacc96869f6b2a32b648299f87188d094
SublimeText1 <510590755@qq.com>
2017-07-18 15:01:23 +0800

    Signed-off-by: SublimeText1 <510590755@qq.com>

--- js/company.js
+++ js/company.js
@@ -4,12 +4,18 @@ $("#sure").validate({
 		companyName:"required",
 		contacts:"required",
 		mobile:"required",
+		eml:{
+			email:true,
+		},
 		leavMessage:"required"
 	},
 	messages:{
 		companyName:"请填写公司名称",
 		contacts:"请填写联系人",
 		mobile:"请填写手机号",
+		eml:{
+			email:"邮箱格式不正确",
+		},
 		leavMessage:"请填写留言",
 	}
 });
