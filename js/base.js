a8569c8df227c7e65cbaab85bcb940b6000288db
sunwarm12 <johndoe@example.c>
2017-07-27 20:44:15 +0800

    4

--- js/base.js
+++ js/base.js
@@ -1,47 +1,28 @@
-// function fun(ele){
-		// 	var details = $("#cake_meau");
-		// 	// details.css("display","none")
-		// 	if (details.is(":visible")==false) {
-		// 		// details.css("display","block");
-		// 		details.is(":visible");
-		// 		$(ele).attr("src","../images/hide.png");
-		// 		// console.log(details);
-		// 	}else if(details.is(":hidden")==false){
-		// 		console.log("===+--=-=-=");
-		// 		details.is(":visible");
-		// 		$(ele).attr("src","../images/hide.png");
-		// 	}else{
-		// 		details.is(":visible");
-		// 	}
-			
-		// }
 function fun(ele){
-	var details = document.getElementById("cake_meau");
-		if (details.style.display=="none") {
-				details.style.display="block";
-				ele.setAttribute("src","../images/hide.png");
-				console.log(details);
-		}else if(details.style.display=="block"){
-				// console.log("===+--=-=-=");
-				details.style.display="none";
-				ele.setAttribute("src","../images/top.png");
+	var details = $("#cake_meau");
+		if (details.css("display")=="none") {
+				details.css("display","block");
+				$(ele).attr("src","../images/hide.png");
+		}else if(details.css("display")=="block"){
+				details.css("display","none");
+				$(ele).attr("src","../images/top.png");
 		}else{
-				details.style.display="none";
-		}
-			
+				details.css("display","none");
+		}		
 	}
 		function fun2(){
-		var main = document.getElementById("cake_box");
-		 var small =document.getElementById("select_city");
-			if (small.style.display =="none"){
-				small.style.opacity = "1";
-				small.style.display = "block";
+			var main=$("#cake_box");
+			var small=$("#select_city");
+		if (small.css("display") =="none"){
+				small.css("display","block");
+				main.css("opacity","0.3");
+				// main.css("background","yellow");
+				// small.css("display","block");
 			}else{
-				small.style.display = "block";
-				main.style.opacity = "0.5";
+				small.css("display","none");
+			    main.css("opacity","1");
 			}
 		}
-		
 		 function city(ntn){
 		 	var sure= $("#sure");
 		 	var main=$("#cake_box");
@@ -54,12 +35,13 @@ function fun(ele){
 		 		$("#show").text($(ntn).text());
 		 		context.css("display","none");
 		 		main.css("opacity","1");
-
-		 	})
-	 							
+		 	})						
 			} 
 
-		// }
+
+
+
+	
 			
 
 		
